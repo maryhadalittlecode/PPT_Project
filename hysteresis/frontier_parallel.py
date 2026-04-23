99b20d4f512e5ff6244b0ff3d6e2779215765801
Xie Xinyi <xx242@zoo.cs.yale.edu>
2026-04-23 01:22:21 -0400

    better Coordinate-List Frontier

--- hysteresis/frontier_parallel.py
+++ hysteresis/frontier_parallel.py
@@ -2,60 +2,204 @@ import numpy as np
 import time
 from numba import njit
 
+
 @njit(cache=True)
-def _hysteresis_frontier_kernel(strong_map, weak_map, connectivity):
+def _init_frontier(strong_map, frontier):
+    """
+    Fill frontier with coordinates of strong pixels.
+    Returns frontier size.
+    """
+    h, w = strong_map.shape
+    count = 0
+    for r in range(h):
+        for c in range(w):
+            if strong_map[r, c]:
+                frontier[count, 0] = r
+                frontier[count, 1] = c
+                count += 1
+    return count
+
+
+@njit(cache=True)
+def _hysteresis_frontier_kernel_4(strong_map, weak_map):
     h, w = strong_map.shape
     result = strong_map.copy()
 
-    # Current frontier: pixels we just activated and need to check neighbors for
-    # We use a boolean mask for the 'next' frontier to ensure uniqueness
-    current_frontier = np.argwhere(strong_map)
+    max_size = h * w
 
-    if connectivity == 4:
-        neighbors = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]], dtype=np.int32)
-    else:
-        neighbors = np.array([
-            [-1, 0], [1, 0], [0, -1], [0, 1],
-            [-1, -1], [-1, 1], [1, -1], [1, 1]
-        ], dtype=np.int32)
+    # Preallocate coordinate-list frontiers
+    frontier_cur = np.empty((max_size, 2), dtype=np.int32)
+    frontier_next = np.empty((max_size, 2), dtype=np.int32)
 
-    while len(current_frontier) > 0:
-        # A boolean mask for the next level to prevent redundant neighbor checks
+    cur_size = _init_frontier(strong_map, frontier_cur)
+
+    while cur_size > 0:
+        next_size = 0
         next_mask = np.zeros((h, w), dtype=np.bool_)
-        found_any = False
 
-        for i in range(len(current_frontier)):
-            r = current_frontier[i, 0]
-            c = current_frontier[i, 1]
+        for i in range(cur_size):
+            r = frontier_cur[i, 0]
+            c = frontier_cur[i, 1]
 
-            for j in range(len(neighbors)):
-                nr, nc = r + neighbors[j, 0], c + neighbors[j, 1]
+            # up
+            nr, nc = r - 1, c
+            if nr >= 0:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
 
-                if 0 <= nr < h and 0 <= nc < w:
-                    # If it's a weak edge and not yet part of our result
-                    if weak_map[nr, nc] and not result[nr, nc]:
-                        result[nr, nc] = True
-                        next_mask[nr, nc] = True
-                        found_any = True
+            # down
+            nr, nc = r + 1, c
+            if nr < h:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
 
-        if not found_any:
-            break
+            # left
+            nr, nc = r, c - 1
+            if nc >= 0:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
 
-        current_frontier = np.argwhere(next_mask)
+            # right
+            nr, nc = r, c + 1
+            if nc < w:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+        frontier_cur, frontier_next = frontier_next, frontier_cur
+        cur_size = next_size
 
     return result
 
+
+@njit(cache=True)
+def _hysteresis_frontier_kernel_8(strong_map, weak_map):
+    h, w = strong_map.shape
+    result = strong_map.copy()
+
+    max_size = h * w
+
+    # Preallocate coordinate-list frontiers
+    frontier_cur = np.empty((max_size, 2), dtype=np.int32)
+    frontier_next = np.empty((max_size, 2), dtype=np.int32)
+
+    cur_size = _init_frontier(strong_map, frontier_cur)
+
+    while cur_size > 0:
+        next_size = 0
+        next_mask = np.zeros((h, w), dtype=np.bool_)
+
+        for i in range(cur_size):
+            r = frontier_cur[i, 0]
+            c = frontier_cur[i, 1]
+
+            # 8 neighbors, manually unrolled
+
+            nr, nc = r - 1, c
+            if nr >= 0:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+            nr, nc = r + 1, c
+            if nr < h:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+            nr, nc = r, c - 1
+            if nc >= 0:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+            nr, nc = r, c + 1
+            if nc < w:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+            nr, nc = r - 1, c - 1
+            if nr >= 0 and nc >= 0:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+            nr, nc = r - 1, c + 1
+            if nr >= 0 and nc < w:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+            nr, nc = r + 1, c - 1
+            if nr < h and nc >= 0:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+            nr, nc = r + 1, c + 1
+            if nr < h and nc < w:
+                if weak_map[nr, nc] and (not result[nr, nc]) and (not next_mask[nr, nc]):
+                    result[nr, nc] = True
+                    next_mask[nr, nc] = True
+                    frontier_next[next_size, 0] = nr
+                    frontier_next[next_size, 1] = nc
+                    next_size += 1
+
+        frontier_cur, frontier_next = frontier_next, frontier_cur
+        cur_size = next_size
+
+    return result
+
+
 def hysteresis(strong_map, weak_map, connectivity=8):
-    """
-    Frontier-based BFS Hysteresis (Level-Synchronous).
-    Optimized with Numba.
-    """
-    # Ensure contiguous arrays for Numba speed
     strong_map = np.ascontiguousarray(strong_map.astype(np.bool_))
     weak_map = np.ascontiguousarray(weak_map.astype(np.bool_))
 
     start = time.perf_counter()
-    result = _hysteresis_frontier_kernel(strong_map, weak_map, connectivity)
-    elapsed = time.perf_counter() - start
 
+    if connectivity == 4:
+        result = _hysteresis_frontier_kernel_4(strong_map, weak_map)
+    elif connectivity == 8:
+        result = _hysteresis_frontier_kernel_8(strong_map, weak_map)
+    else:
+        raise ValueError("connectivity must be 4 or 8")
+
+    elapsed = time.perf_counter() - start
     return result, elapsed
